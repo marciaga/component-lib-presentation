c8b525a6a65deb3973933935dd41c1d9c83c6db6
Mark Arciaga <marciaga@flightstats.com>
2017-10-09 21:21:04 -0700

    edit last slide

--- presentation/slides/10/index.js
+++ presentation/slides/10/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, ListItem, Heading, Slide } from 'spectacle';
+import { Link, Text, List, ListItem, Heading, Slide } from 'spectacle';
 
 export default (
   <Slide>
@@ -13,5 +13,39 @@ export default (
       <ListItem>Get this thing publishable</ListItem>
       <ListItem>Profit</ListItem>
     </List>
+    <Text>
+      This presentation uses Spectacle and the source code is&nbsp;
+      <Link
+        href="https://github.com/marciaga/component-lib-presentation"
+        target="_blank"
+      >
+        here.
+      </Link>
+    </Text>
+    <Text>
+      The React Cosmos demo source code is&nbsp;
+      <Link
+        href="https://github.com/marciaga/cosmos-demo"
+        target="_blank"
+      >
+        here
+      </Link>
+      &nbsp;and a live version is&nbsp;
+      <Link
+        href="https://marciaga.github.io/cosmos-demo/"
+        target="_blank"
+      >
+        here.
+      </Link>
+    </Text>
+    <Text>
+      Contact me at mark.arciaga@gmail.com, find me on&nbsp;
+      <Link
+        href="https://github.com/marciaga"
+        target="_blank"
+      >
+        GitHub
+      </Link>
+    </Text>
   </Slide>
 );
